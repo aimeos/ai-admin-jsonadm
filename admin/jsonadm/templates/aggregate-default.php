f4612de7e4e4dcd0253092f6514a5ddd607b299b
Aimeos <aimeos@aimeos.org>
2016-06-28 19:34:57 +0200

    Fixed missing commas in JSON output

--- admin/jsonadm/templates/aggregate-default.php
+++ admin/jsonadm/templates/aggregate-default.php
@@ -1,6 +1,11 @@
 <?php
 
 $data = $this->get( 'data', array() );
+$entries = array();
+
+foreach( $data as $key => $value ) {
+	$entries[] = array( 'id' => $key, 'type' => 'aggregate', 'attributes' => $value );
+}
 
 ?>
 {
@@ -11,10 +16,6 @@ $data = $this->get( 'data', array() );
 <?php	if( isset( $this->errors ) ) : ?>
 	"errors": <?php echo $this->partial( $this->config( $this->get( 'partial-errors', 'admin/jsonadm/partials/template-errors' ), 'partials/errors-standard.php' ), array( 'errors' => $this->errors ) ); ?>
 <?php	elseif( isset( $this->data ) ) : ?>
-	"data": [
-<?php		foreach( $data as $key => $value ) : ?>
-<?php			echo json_encode( array( 'id' => $key, 'type' => 'aggregate', 'attributes' => $value ) ); ?>
-<?php	 	endforeach; ?>
-    ]
+	"data": <?php echo json_encode( $entries ); ?>
 <?php	endif; ?>
 }
