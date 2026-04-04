102a7e31c532798e744f2dd87e29d355fd493166
Aimeos <aimeos@aimeos.org>
2026-04-04 09:58:08 +0200

    Fixed deprecation

--- tests/Admin/JsonAdm/Common/Decorator/BaseTest.php
+++ tests/Admin/JsonAdm/Common/Decorator/BaseTest.php
@@ -103,7 +103,6 @@ class BaseTest extends \PHPUnit\Framework\TestCase
 	{
 		$class = new \ReflectionClass( \Aimeos\Admin\JsonAdm\Common\Decorator\Base::class );
 		$method = $class->getMethod( $name );
-		$method->setAccessible( true );
 
 		return $method;
 	}
